4eb235a126dd1ad432d261e11dced6bf03c08ad5
Tuomo <tuokri@tuta.io>
2022-08-19 15:48:00 +0300

    Fix versioning

---
+++ rs2simlib/__init__.py
@@ -0,0 +1,15 @@
+from pathlib import Path as _Path
+
+__all__ = [
+    "version",
+    "__version__",
+    "__version_tuple__",
+]
+
+_version = {}
+with open(f"{_Path(__file__).parent}/_version.py") as _fp:
+    exec(_fp.read(), _version)
+
+__version__ = _version["__version__"]
+__version_tuple__ = _version["__version_tuple__"]
+version = __version__
