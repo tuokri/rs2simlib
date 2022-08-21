30a93d12e3035a871dcef0647cede0582074a4ba
Tuomo <tuokri@tuta.io>
2022-08-22 00:49:19 +0300

    Add more models to module interface

--- rs2simlib/models/__init__.py
+++ rs2simlib/models/__init__.py
@@ -1,17 +1,25 @@
+from .models import Bullet
 from .models import BulletParseResult
 from .models import ClassBase
 from .models import DragFunction
 from .models import PROJECTILE
+from .models import ParseResult
 from .models import WEAPON
 from .models import Weapon
 from .models import WeaponParseResult
+from .models import WeaponSimulation
+from .models import interp_dmg_falloff
 
 __all__ = [
+    "Bullet",
     "BulletParseResult",
     "ClassBase",
     "DragFunction",
     "PROJECTILE",
+    "ParseResult",
     "WEAPON",
     "Weapon",
     "WeaponParseResult",
+    "WeaponSimulation",
+    "interp_dmg_falloff",
 ]
