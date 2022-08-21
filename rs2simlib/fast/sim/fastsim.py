377a11f13f700e2a2b6d94a18a59148ef135142d
Tuomo <tuokri@tuta.io>
2022-08-21 14:50:34 +0300

    Finish fastsim implementation

--- rs2simlib/fast/sim/fastsim.py
+++ rs2simlib/fast/sim/fastsim.py
@@ -1,41 +1,91 @@
-from typing import Tuple
+import math
 
 import numba as nb
 import numpy as np
+import numpy.typing as npt
 
 from rs2simlib.fast.drag import drag_g1
 from rs2simlib.fast.drag import drag_g7
 
-SCALE_FACTOR_INVERSE: nb.float32 = 0.065618
-SCALE_FACTOR: nb.float32 = 15.24
+SCALE_FACTOR_INVERSE = nb.float64(0.065618)
+SCALE_FACTOR = nb.float64(15.24)
+
+
+@nb.njit
+def clamp(n: nb.float64,
+          smallest: nb.float64,
+          largest: nb.float64) -> nb.float64:
+    return max(smallest, min(n, largest))
+
+
+@nb.njit
+def calc_damage(
+        velocity: npt.NDArray[np.float64],
+        muzzle_velocity: nb.float64,
+        damage: nb.int32,
+        falloff_x: npt.NDArray,
+        falloff_y: npt.NDArray,
+) -> nb.float64:
+    v_size_sq = np.linalg.norm(velocity) ** 2
+    power_left = v_size_sq / (muzzle_velocity ** 2)
+    damage *= power_left
+    energy_transfer = np.interp(
+        x=v_size_sq,
+        xp=falloff_x,
+        fp=falloff_y,
+    )
+    energy_transfer = clamp(
+        energy_transfer,
+        smallest=falloff_y[0],
+        largest=falloff_y[-1],
+    )
+    return damage * energy_transfer
 
 
 @nb.njit
 def simulate(
-        sim_time: nb.float32,
-        time_step: nb.float32,
+        sim_time: nb.float64,
+        time_step: nb.float64,
         drag_func: nb.int32,
-        ballistic_coeff: nb.float32,
-        aim_dir_x: nb.float32,
-        aim_dir_y: nb.float32,
-        muzzle_velocity: nb.float32,
-        start_loc_x: nb.float32 = 0.0,
-        start_loc_y: nb.float32 = 0.0,
-) -> Tuple[np.ndarray, np.ndarray]:
-    v_size: nb.float32
-    mach: nb.float32
-    v: nb.float32
-    cd: nb.float32
-    flight_time: nb.float32 = 0.0
-    distance_traveled_uu: nb.float32 = 0.0
-    location = np.array([start_loc_x, start_loc_y])
+        ballistic_coeff: nb.float64,
+        aim_dir_x: nb.float64,
+        aim_dir_y: nb.float64,
+        muzzle_velocity: nb.float64,
+        falloff_x: npt.NDArray,
+        falloff_y: npt.NDArray,
+        bullet_damage: nb.int32,
+        instant_damage: nb.int32,
+        pre_fire_trace_len: nb.int32,
+        start_loc_x: nb.float64 = 0.0,
+        start_loc_y: nb.float64 = 0.0,
+) -> npt.NDArray[npt.NDArray[np.float64]]:
+    d: nb.float64
+    v_size: nb.float64
+    mach: nb.float64
+    v: nb.float64
+    cd: nb.float64
+    damage: nb.float64
+    flight_time = nb.float64(0.0)
+    location = np.array([start_loc_x, start_loc_y], dtype=np.float64)
+    prev_loc = location.copy()
     bc_inverse = 1.0 / ballistic_coeff
-    velocity = np.array([aim_dir_x, aim_dir_y])
+    velocity = np.array([aim_dir_x, aim_dir_y], np.float64)
     velocity /= np.linalg.norm(velocity)
     velocity *= muzzle_velocity
-    drag_func = drag_g7 if (drag_func == 7) else drag_g1
 
-    while flight_time < sim_time:
+    drag_func = drag_g7 if (drag_func == 7) else drag_g1
+    num_steps = math.ceil(sim_time / time_step)
+    arr_len = num_steps - 1
+
+    trajectory_x = np.empty(arr_len, dtype=np.float64)
+    trajectory_y = np.empty(arr_len, dtype=np.float64)
+    damage = np.empty(arr_len, dtype=np.float64)
+    distance = np.empty(arr_len, dtype=np.float64)
+    time_at_flight = np.empty(arr_len, dtype=np.float64)
+    bullet_velocity = np.empty(arr_len, dtype=np.float64)
+
+    i = nb.int32(0)
+    while (flight_time < sim_time) and (i <= arr_len):
         flight_time += time_step
         v_size = np.linalg.norm(velocity)
         v = v_size * SCALE_FACTOR_INVERSE
@@ -48,9 +98,36 @@ def simulate(
                 * (-1 * (velocity / v_size * time_step)))
         velocity[1] -= (490.3325 * time_step)
         loc_change = velocity * time_step
-        prev_loc = location.copy()
+        prev_loc[0] = location[0]
+        prev_loc[1] = location[1]
         location += loc_change
-        distance_traveled_uu += abs(
-            np.linalg.norm(prev_loc - location))
+        d = abs(np.linalg.norm(prev_loc - location))
+        distance[i] += d
+        if d <= pre_fire_trace_len:
+            damage[i] = instant_damage
+        else:
+            damage[i] = calc_damage(
+                velocity,
+                muzzle_velocity,
+                bullet_damage,
+                falloff_x,
+                falloff_y)
+        trajectory_x[i] = location[0]
+        trajectory_y[i] = location[1]
+        time_at_flight[i] = flight_time
+        bullet_velocity[i] = np.linalg.norm(velocity)
+        i += 1
 
-    return np.zeros(0), np.zeros(0)
+    trajectory_x /= 50
+    trajectory_y /= 50
+    velocity /= 50
+    distance /= 50
+
+    ret = np.empty(shape=(6, arr_len), dtype=np.float64)
+    ret[0] = trajectory_x
+    ret[1] = trajectory_x
+    ret[2] = damage
+    ret[3] = distance
+    ret[4] = time_at_flight
+    ret[5] = bullet_velocity
+    return ret
--- tests/test_sim.py
+++ tests/test_sim.py
@@ -1,15 +1,32 @@
+import math
+
+import numpy as np
+
 from rs2simlib.fast import sim as fastsim
 
 
 def test_fast_simulate():
-    sim_x, sim_y = fastsim.simulate(
-        sim_time=5.0,
-        time_step=1 / 500,
+    time_step = 1 / 500
+    sim_time = 5.0
+
+    sim_results = fastsim.simulate(
+        sim_time=sim_time,
+        time_step=time_step,
         drag_func=7,
-        ballistic_coeff=0.25,
+        ballistic_coeff=0.24,
         aim_dir_x=1.0,
         aim_dir_y=0.0,
-        muzzle_velocity=700.0 * 50,
+        muzzle_velocity=340.0 * 50,
+        falloff_x=np.array([241491600.0, 1509322500.0]),
+        falloff_y=np.array([0.85, 0.2]),
+        bullet_damage=147,
+        instant_damage=160,
+        pre_fire_trace_len=25 * 50,
         start_loc_x=0.0,
         start_loc_y=0.0,
     )
+
+    arr_len = math.ceil(sim_time / time_step) - 1
+    assert np.size(sim_results) == arr_len * 6
+    assert np.size(sim_results, axis=0) == 6
+    assert np.size(sim_results, axis=1) == arr_len
