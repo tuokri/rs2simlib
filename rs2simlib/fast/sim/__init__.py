080f74f86c96b80e097b8dfb466682e60ae8d2b9
Tuomo <tuokri@tuta.io>
2024-07-22 12:03:14 +0300

    Expose all functions in fastsim.py

--- rs2simlib/fast/sim/__init__.py
+++ rs2simlib/fast/sim/__init__.py
@@ -1,7 +1,13 @@
+from .fastsim import calc_damage
+from .fastsim import calc_energy_transfer
+from .fastsim import calc_power_left
 from .fastsim import simulate
 from .fastsim import trigger_jit
 
 __all__ = [
+    "calc_damage",
+    "calc_energy_transfer",
+    "calc_power_left",
     "simulate",
     "trigger_jit",
 ]
